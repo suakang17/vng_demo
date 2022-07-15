d554ccc4b2fba01fe0700cc9e8cdee5929b6cb8c
suakang17 <rkdtndk09@gmail.com>
2022-07-15 13:44:00 +0900

    feat:Review entity

--- src/main/java/domain/review/Review.java
+++ src/main/java/domain/review/Review.java
@@ -1,4 +1,19 @@
 package domain.review;
 
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "review")
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reviewId")
+    private int reviewId;
+
+
 }
