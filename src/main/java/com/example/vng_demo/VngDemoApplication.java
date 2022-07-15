f5ae4c344f7ab7c8ddc61459476629cc62050e9d
suakang17 <rkdtndk09@gmail.com>
2022-07-15 12:15:36 +0900

    Initial commit

---
+++ src/main/java/com/example/vng_demo/VngDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.vng_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VngDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VngDemoApplication.class, args);
+	}
+
+}
