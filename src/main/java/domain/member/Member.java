c0f10b9a2d44d27b5464cb5f9f7fc8a4426b94d8
suakang17 <rkdtndk09@gmail.com>
2022-07-18 22:09:49 +0900

    chore:entity typo

--- src/main/java/domain/member/Member.java
+++ src/main/java/domain/member/Member.java
@@ -1,31 +1,52 @@
 package domain.member;
-
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
-
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
-@Table( name = "member")
+@Table(name = "member")
+@Getter @Setter
 public class Member {
     //필드
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoIncrement를 위한 필수 옵션
-    @Column(name = "userId")
-    private int userId;
+    //@GeneratedValue(strategy = GenerationType.IDENTITY) //autoIncrement를 위한 필수 옵션
+    @Column(name = "userId", nullable = false)
+    private String userId;
 
-    @Column(name = "name", columnDefinition = "nvarchar")
+    @Column(name = "name", columnDefinition = "nvarchar", nullable = false)
     private String name;
 
-    @Column(name = "nickName", columnDefinition = "nvarchar")
+    @Column(name = "nickName", columnDefinition = "nvarchar", nullable = false)
     private String nickName;
 
+    @Column(name = "gender", nullable = false)
     private char gender;
+
+    @Column(name = "age", nullable = false)
     private int age;
+
+    @Column(name = "standingHeight", nullable = false)
     private int standingHeight;
+
+    @Column(name = "sittingHeight", nullable = false)
     private int sittingHeight;
 
+    @OneToMany(mappedBy = "member")
+    private List<Scrap> scrapList;
 
-
+    public Member(String userId, String name, String nickName, char gender, int age, int standingHeight, int sittingHeight) {
+        this.age = age;
+        this.gender = gender;
+        this.name = name;
+        this.nickName = nickName;
+        this.sittingHeight = sittingHeight;
+        this.standingHeight = standingHeight;
+    }
 }
+
+
--- src/main/java/domain/review/Review.java
+++ src/main/java/domain/review/Review.java
@@ -1,19 +1,53 @@
 package domain.review;
 
+import domain.member.Member;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Table(name = "review")
+@Getter @Setter
 public class Review {
-
+    //필드
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "reviewId")
-    private int reviewId;
+    @Column(name = "reviewId", nullable = false)
+    private Long reviewId;
 
+    @JoinColumn(name = "userId") //FK
+    private Member writer;
 
+    @JoinColumn(name = "placeId") //FK venue
+    private Place place;
+
+    @JoinColumn(name = "performanceId") //FK show
+    private Performance performance;
+
+    @JoinColumn(name = "artistId") //FK artist
+    private Artist artist;
+
+    //seat
+
+    @Column(name = "rating") //evaluation
+    private float rating;
+
+    @Column(name = "writingTime")
+    private LocalDateTime writingTime;
+
+    @Column(name = "photoReview", columnDefinition = "varchar") //pictures
+    private String photoReview;
+
+    public Review (Long reviewId, float rating, LocalDateTime writingTime, String photoReview) {
+        this.photoReview = photoReview;
+        this.rating = rating;
+        this.reviewId = reviewId;
+        this.writingTime = writingTime;
+
+    }
 }
